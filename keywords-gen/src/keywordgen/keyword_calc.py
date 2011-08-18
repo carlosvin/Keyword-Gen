9c491f2f6ce8bc87eb2303ac7a597252742fe21c
carlosvin <carlosvin@gmail.com>
2011-08-18 13:59:13 +0200

    The algorith is developt using re module

--- keywords-gen/src/keywordgen/keyword_calc.py
+++ keywords-gen/src/keywordgen/keyword_calc.py
@@ -3,41 +3,38 @@ Created on 06/08/2011
 
 @author: carlos
 '''
+import re
+
 def element_cmp (a, b):
     return a(0) < b(0)
 
-def apply_separators (text, separators, return_list):
-    if separators == None or separators == []:
-        return_list.append( text )
-    else:
-        separators_cp = separators
-        for separator in separators:
-            splitted_text = text.split ( separator )
-            separators_cp.remove ( separator )
-            for splitted_word in splitted_text:
-                apply_separators (splitted_word, separators_cp, return_list )
-            
 
-def get_keywords (text, num, separators):
-    list_of_words = []
+def get_keywords (text, exclusion_list):
+    
     keywords = []
+        
+    list_of_words = re.split('\W+', text )
     
-    for splitted_text in text.split():
-        apply_separators(splitted_text, separators, list_of_words)
-    
-    print(list_of_words)
-    
+    for exclusion_word in exclusion_list:
+        try:
+            list_of_words.remove (exclusion_word)
+        except ValueError:
+            pass
+        
     words_count = []
     for word in list_of_words:
         words_count.append( (list_of_words.count( word ), word) )
+        salir = False
+        while (not salir):
+            try:
+                list_of_words.remove( word )
+            except ValueError:
+                salir = True
+        print(list_of_words)
+        
     words_count.sort( key=lambda word_count : word_count[0], reverse = True )
     
     for word in words_count:
-        if keywords.count(word[1]):
-            keywords.append( word[1] )
+        keywords.append( word[1] )
     
     return keywords
-
-#if __name__ == '__main__':
-#    print (get_keywords ("El perro de San Roque no tiene rabo, perro perro perro, San", 10, [","," ", "de"]))
-    
--- keywords-gen/src/keywordgen/test/test.py
+++ keywords-gen/src/keywordgen/test/test.py
@@ -5,12 +5,12 @@ Created on 16/08/2011
 '''
 import unittest
 from keywordgen.keyword_calc import get_keywords
-
+import string
 
 class Test(unittest.TestCase):
     
     def testKeyWordGen(self):
-        keywords = get_keywords ("El perro de San Roque no tiene rabo, perro perro perro, San", 10, [",", "de"])
+        keywords = get_keywords ("El perro de San Roque no tiene rabo, perro perro perro, San", [",", "de", "no", "a", "El"])
         print (keywords)
 
 if __name__ == "__main__":
